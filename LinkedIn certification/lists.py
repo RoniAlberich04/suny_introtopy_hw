f56dd7fd5cded85a800c06549923656d54dfc414
RoniAlberich04 <angelinafrost28@gmail.com>
2025-11-17 17:43:26 +0100

    Added hw 5

---
+++ LinkedIn certification/lists.py
@@ -0,0 +1,11 @@
+my_list = [1, 2, [2.1, 2.2, 2.3], 3, 4, 5] #list - список
+
+print(my_list)
+
+my_set = {1, 1, 2, 2, 5} #set - сэт
+
+print(len(my_set))
+
+my_tuple = (1, 2, 3, 4)
+
+#Attribute Error - ошибка для атрибута. 
